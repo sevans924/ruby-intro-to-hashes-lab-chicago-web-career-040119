a7afe7b359aebfbd3bd6b168d4a03593f7655cb0
Sarah Elzinga <sevans924@gmail.com>
2019-03-24 21:23:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,9 @@ def monopoly_with_third_tier
 			:pieces => 4,
 		  :names => {},
 		  :rent_in_dollars => {
-				:one_piece_owned => 25}
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100}
 		}
 	}#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
